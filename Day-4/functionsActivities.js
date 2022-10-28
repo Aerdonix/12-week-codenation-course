6b41117f607678aaf85331632cafc4b12a7a7feb
Jack <badpixel71@gmail.com>
2022-10-28 11:22:13 +0100

    removed unneccesary code

--- Day-4/functionsActivities.js
+++ Day-4/functionsActivities.js
@@ -12,22 +12,16 @@
 
 // Activity 2
 
-let orderCount = 0;
+// let orderCount = 0;
 
-const takeOrder =(topping, drink) => {
-    console.log (`Pizza with ${topping} and ${drink}`);
-    orderCount++;
-}
-let orderPrice = 0;
+// const takeOrder =(topping, drink) => {
+//     console.log (`Pizza with ${topping} and ${drink}`);
+//     orderCount++;
+// }
+// let orderPrice = 0;
 
-const totalPrice = (price) => {
-    console.log(`${price}`);
-    orderPrice++;
-}
+// takeOrder("chicken", "pepsi");
 
-takeOrder("chicken", "pepsi");
 
-totalPrice("")
+// console.log (`${orderCount} total orders.`)
 
-console.log (`${orderCount} total orders.`)
-console.log (`${orderPrice} for this order.`)
